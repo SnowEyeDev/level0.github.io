0daf750c7a4e7f33adf077e9527381c6c87ed091
Michielvk <16121929+Michielvk@users.noreply.github.com>
2023-01-08 18:46:11 +0100

    Fix:  invalid type warning

--- level0/components/NavBar.vue
+++ level0/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="flex justify-center gap-10">
-        <NavBarItem item-num="0" item-text="hello" />
-        <NavBarItem item-num="1" item-text="about" />
-        <NavBarItem item-num="2" item-text="contact" />
+        <NavBarItem :item-num="0" item-text="hello" />
+        <NavBarItem :item-num="1" item-text="about" />
+        <NavBarItem :item-num="2" item-text="contact" />
     </nav>
 </template>
 
