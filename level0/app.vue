1993aa170343d25c8155868938e9223ee3b15197
Michielvk <16121929+Michielvk@users.noreply.github.com>
2023-01-15 16:13:10 +0100

    feat: add simple page transition

--- level0/app.vue
+++ level0/app.vue
@@ -4,3 +4,16 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.25s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.2rem);
+}
+</style>
